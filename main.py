ca1782ce950defa4e2c271d28f27886f1733add1
RodinaMohamed04 <es.rodaina.mohamed2023@alexu.edu.eg>
2026-07-24 18:35:56 +0300

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 
@@ -48,3 +48,41 @@ def create_task(task: TaskCreate):
     new_task = {"id": len(tasks) + 1, "title": task.title, "done": False}
     tasks.append(new_task)
     return new_task
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    done: bool | None = None
+
+@app.put("/tasks/{id}")
+def update_task(id: int, task: TaskUpdate):
+    if (task.title is None and task.done is None):
+        return JSONResponse(
+            status_code=400,
+            content={"error": "The body cannot be empty"}
+        )
+    if (task.title is not None and not task.title.strip()):
+        return JSONResponse(
+            status_code=400,
+            content={"error": "Title cannot be empty"}
+        )
+    for t in tasks:
+        if t["id"] == id:
+            if task.title is not None:
+                t["title"] = task.title
+            if task.done is not None:
+                t["done"] = task.done
+            return t
+        return JSONResponse(
+            status_code=404,
+            content={"error": f"Task {id} not found"}
+        )
+
+@app.delete("/tasks/{id}", status_code=204)
+def delete_task(id: int):
+    for t in tasks:
+        if t["id"] == id:
+            tasks.remove(t)
+            return Response(status_code=204)
+        return JSONResponse(
+            status_code=404,
+            content={"error": f"Task {id} not found"}
+        )
